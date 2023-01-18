1cad90acfde37a457c50b871a877440acf3222bd
Andrewax92 <andreiaxinoi1@gmail.com>
2023-01-18 09:14:29 -0600

    Adding the Nav componentn

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./pages/home/Home"
 import Create from "./pages/create/Create"
 import Search from './pages/search/Search';
 import Recipe from './pages/recipe/Recipe';
+import Navbar from './components/Navbar';
 
 
 import './App.css'
@@ -17,6 +18,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create" element={<Create />} />
